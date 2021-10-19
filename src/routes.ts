039265c6a8f198a18b93579fc1ad36bad251617e
RayanPhablo <phablorayan02@gmail.com>
2021-10-19 20:28:13 -0300

    commit on main

---
+++ src/routes.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateMessageController } from "./controllers/CreateMessageController";
+import { GetLastThreeMessagesController } from "./controllers/GetLasThreeMessagesController";
+import { ProfileUserController } from "./controllers/ProfileUserController";
+import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
+
+const router = Router();
+
+router.post("/authenticate", new AuthenticateUserController().handle);
+
+router.post("/messages", ensureAuthenticated, new CreateMessageController().handle);
+
+router.get("/messages/last3", new GetLastThreeMessagesController().handle);
+
+router.get("/profile", new ProfileUserController().handle);
+
+export { router };
